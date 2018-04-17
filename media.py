19c37a06baa90083409ce05aa1fab371a7909ad9
guomxin <guomaoxin@foxmail.com>
2018-04-17 19:54:06 +0800

    add more content to README

--- media.py
+++ media.py
@@ -1,9 +1,7 @@
 #-*- encoding: utf-8 -*-
 
 class Movie():
-    """
-    本类存储电影相关的信息，存储的信息包括：电影名、海报地址、预告片Youtube地址
-    """
+    #本类存储电影相关的信息，存储的信息包括：电影名、海报地址、预告片Youtube地址
 
     def __init__(self, title, poster_image_url, trailer_youtube_url):
         self.title = title
